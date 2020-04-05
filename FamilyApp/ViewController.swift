2bc0c960478095331b148b02b4cd2ba2c84898fe
keiMura <ke_stupidman01@yahoo.co.jp>
2020-04-05 17:14:24 +0900

    Initial Commit

---
+++ FamilyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FamilyApp
+//
+//  Created by 村上圭 on 2020/04/05.
+//  Copyright © 2020 村上圭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FamilyAppTests/FamilyAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  FamilyAppTests.swift
+//  FamilyAppTests
+//
+//  Created by 村上圭 on 2020/04/05.
+//  Copyright © 2020 村上圭. All rights reserved.
+//
+
+import XCTest
+@testable import FamilyApp
+
+class FamilyAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
